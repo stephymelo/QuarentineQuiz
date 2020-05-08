5426f7fc0cdde98f882509702845f410170791d3
stephym <60659457+stephymelo@users.noreply.github.com>
2020-05-08 09:04:04 -0500

    setupQuiz

---
+++ src/model/Main.java
@@ -0,0 +1,22 @@
+package model;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+	public static void main(String[] args) {
+		PApplet.main("model.Main");
+
+	}
+	
+	public void settings() {
+		size(500,500);
+
+	}
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+}
